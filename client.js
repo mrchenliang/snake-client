ea1f9d52b21875bd4c85e5e15a84b86026307614
Chen Liang <c.liang@ualberta.ca>
2019-07-31 18:44:27 +0000

    snake game with user input

--- client.js
+++ client.js
@@ -9,10 +9,18 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding("utf8");
-  conn.on("data", data => {
-    console.log("Server says: ", data);
-  });
+  conn.on("connect", () => {
+    console.log("connected!!");
+    conn.write("Name: S3X \n");
+      // conn.write("Move: up");
+      // conn.write("Move: up");
+    conn.on("data", data => {
+      console.log("Server says: ", data);
+      });
+    });
   return conn;
 };
+
+// Name: SNK
 
 module.exports = { connect };
--- play.js
+++ play.js
@@ -1,5 +1,29 @@
-
 const { connect } = require('./client');
-console.log('Connecting ...');
-connect();
+let conn;
 
+const handleUserInput = function(data) {
+  if (data === '\u0003') {
+    process.exit();
+  } else if (data === 'w') {
+    conn.write('Move: up');
+  } else if (data === 'a') {
+    conn.write('Move: left');
+  } else if (data === 's') {
+    conn.write('Move: down');
+  } else if (data === 'd') {
+    conn.write('Move: right');
+  }
+}
+
+const setupInput = function(connection) {
+  conn = connection;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+console.log('Connecting ...');
+setupInput(connect());
